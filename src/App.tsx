cdbcf768422a3c45c4e1272e167c0a1fba519c8a
xiejayu2024-hue <xiejayu2024-hue@users.noreply.github.com>
2026-06-02 10:16:29 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { useEffect } from 'react'
+import { initStore } from './store'
+import { useStore } from './store'
+import { buildSettingsFromUrlParams, clearUrlSettingParams, hasUrlSettingParams } from './lib/urlSettings'
+import { useDockerApiUrlMigrationNotice } from './hooks/useDockerApiUrlMigrationNotice'
+import Header from './components/Header'
+import SearchBar from './components/SearchBar'
+import TaskGrid from './components/TaskGrid'
+import AgentWorkspace from './components/AgentWorkspace'
+import InputBar from './components/InputBar'
+import DetailModal from './components/DetailModal'
+import Lightbox from './components/Lightbox'
+import SettingsModal from './components/SettingsModal'
+import ConfirmDialog from './components/ConfirmDialog'
+import Toast from './components/Toast'
+import MaskEditorModal from './components/MaskEditorModal'
+import ImageContextMenu from './components/ImageContextMenu'
+import SupportPromptModal from './components/SupportPromptModal'
+import { useGlobalClickSuppression } from './lib/clickSuppression'
+
+export default function App() {
+  const setSettings = useStore((s) => s.setSettings)
+  const appMode = useStore((s) => s.appMode)
+  useDockerApiUrlMigrationNotice()
+  useGlobalClickSuppression()
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search)
+    const nextSettings = buildSettingsFromUrlParams(useStore.getState().settings, searchParams)
+
+    setSettings(nextSettings)
+
+    if (hasUrlSettingParams(searchParams)) {
+      clearUrlSettingParams(searchParams)
+
+      const nextSearch = searchParams.toString()
+      const nextUrl = `${window.location.pathname}${nextSearch ? `?${nextSearch}` : ''}${window.location.hash}`
+      window.history.replaceState(null, '', nextUrl)
+    }
+
+    initStore()
+  }, [setSettings])
+
+  useEffect(() => {
+    const preventPageImageDrag = (e: DragEvent) => {
+      if ((e.target as HTMLElement | null)?.closest('img')) {
+        e.preventDefault()
+      }
+    }
+
+    document.addEventListener('dragstart', preventPageImageDrag)
+    return () => document.removeEventListener('dragstart', preventPageImageDrag)
+  }, [])
+
+  return (
+    <>
+      <Header />
+      {appMode === 'agent' ? (
+        <AgentWorkspace />
+      ) : (
+        <main data-home-main data-drag-select-surface className="pb-48">
+          <div className="safe-area-x max-w-7xl mx-auto">
+            <SearchBar />
+            <TaskGrid />
+          </div>
+        </main>
+      )}
+      <InputBar />
+      <DetailModal />
+      <Lightbox />
+      <SettingsModal />
+      <ConfirmDialog />
+      <SupportPromptModal />
+      <Toast />
+      <MaskEditorModal />
+      <ImageContextMenu />
+    </>
+  )
+}

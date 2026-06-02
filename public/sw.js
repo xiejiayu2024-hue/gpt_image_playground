594c92f231b9279d53148206d183eb5232ede04c
xiejayu2024-hue <xiejayu2024-hue@users.noreply.github.com>
2026-06-02 11:02:56 +0800

    Update project logo

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'gpt-image-playground-v0.1.5'
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './pwa-icon.svg']
+const CACHE_NAME = 'gpt-image-playground-v0.1.6'
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './pwa-icon.png']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
